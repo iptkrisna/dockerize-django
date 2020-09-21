0b46f51763689d94bb54d9bc9d23312c7aa8fcc4
iptkrisna <krisnamath9@gmail.com>
2020-09-21 22:52:58 +0800

    update user data

--- app/app/urls.py
+++ app/app/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers, serializers, viewsets
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['url', 'username', 'first_name', 'last_name', 'email', 'is_staff']
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
